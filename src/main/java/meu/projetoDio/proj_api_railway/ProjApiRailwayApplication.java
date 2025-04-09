f09e0f0e62c75b5e9e60536d4ddf6d2c73f8ef5e
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-04-09 13:38:28 -0300

    Initial commit

---
+++ src/main/java/meu/projetoDio/proj_api_railway/ProjApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package meu.projetoDio.proj_api_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjApiRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meu/projetoDio/proj_api_railway/ProjApiRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package meu.projetoDio.proj_api_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApiRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
